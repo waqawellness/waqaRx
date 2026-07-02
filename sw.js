611c213df53f5adc688aa2b4087fff698a40d14d
waqawellness <waqawellness@gmail.com>
2026-07-02 18:12:45 +0530

    Initial commit
    Files uploaded to github

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = 'waqarx-v2';
+const ASSETS = [
+  './', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './logo.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => Promise.allSettled(ASSETS.map(a => c.add(a)))));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).then(r => {
+      const copy = r.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+      return r;
+    }).catch(() => caches.match(e.request))
+  );
+});
